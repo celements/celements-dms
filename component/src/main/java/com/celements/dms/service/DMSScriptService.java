53e424451c764c725bded9d0e32097c4a638b375
Marc Sladek <marc@sladek.me>
2021-06-21 04:06:48 +0200

    replace commons logger (#1)

--- component/src/main/java/com/celements/dms/service/DMSScriptService.java
+++ component/src/main/java/com/celements/dms/service/DMSScriptService.java
@@ -1,7 +1,5 @@
 package com.celements.dms.service;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.xwiki.component.annotation.Component;
 import org.xwiki.component.annotation.Requirement;
 import org.xwiki.context.Execution;
@@ -12,15 +10,11 @@ import com.xpn.xwiki.XWikiContext;
 @Component("celdms")
 public class DMSScriptService implements ScriptService {
 
-  private static Log LOGGER = LogFactory.getFactory().getInstance(
-      DMSScriptService.class);
-
   @Requirement
   Execution execution;
-  
-  private XWikiContext getContext() {
-    return (XWikiContext)execution.getContext().getProperty("xwikicontext");
-  }
 
+  private XWikiContext getContext() {
+    return (XWikiContext) execution.getContext().getProperty("xwikicontext");
+  }
 
 }
