3b2803db67e6ee2dcc704b95e7826f9cef4829e5
Fabian Pichler <fabian.pichler@synventis.com>
2021-06-27 05:36:13 +0200

    add celements-model dependency, refactor test

--- component/src/test/java/com/celements/dms/service/DMSScriptServiceTest.java
+++ component/src/test/java/com/celements/dms/service/DMSScriptServiceTest.java
@@ -1,29 +1,15 @@
 package com.celements.dms.service;
 
 import static com.celements.common.test.CelementsTestUtils.*;
-import static org.easymock.EasyMock.*;
 import static org.junit.Assert.*;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.xwiki.script.service.ScriptService;
-import org.xwiki.test.AbstractComponentTestCase;
 
-import com.xpn.xwiki.XWiki;
-import com.xpn.xwiki.XWikiContext;
+import com.celements.common.test.AbstractComponentTest;
 import com.xpn.xwiki.web.Utils;
 
-public class DMSScriptServiceTest extends AbstractComponentTestCase {
-
-  private XWiki xwiki;
-  private XWikiContext context;
-
-  @Before
-  public void setUp_DMSScriptServiceTest() throws Exception {
-    xwiki = createMock(XWiki.class);
-    context = getContext();
-    context.setWiki(xwiki);
-  }
+public class DMSScriptServiceTest extends AbstractComponentTest {
 
   @Test
   public void componentTest() {
