6520098858ad4b0c1c6d5c369239b59779fc51d8
Fabian <fabian.pichler@synventis.com>
2021-06-29 17:45:13 +0200

    CELDEV-969 - [Security] May 2021 Upgrade Dependencies (#2)
    * update to celements-5
    
    * add celements-model dependency, refactor test
    
    * add celements-dms component
    
    * remove duplicate group-id

--- component/src/test/java/com/celements/dms/service/DMSScriptServiceTest.java
+++ component/src/test/java/com/celements/dms/service/DMSScriptServiceTest.java
@@ -1,50 +1,21 @@
 package com.celements.dms.service;
 
-
-import static org.easymock.EasyMock.*;
+import static com.celements.common.test.CelementsTestUtils.*;
 import static org.junit.Assert.*;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.xwiki.script.service.ScriptService;
 
-import com.celements.common.test.AbstractBridgedComponentTestCase;
-import com.xpn.xwiki.XWiki;
-import com.xpn.xwiki.XWikiContext;
+import com.celements.common.test.AbstractComponentTest;
 import com.xpn.xwiki.web.Utils;
 
-public class DMSScriptServiceTest extends AbstractBridgedComponentTestCase {
-
-  private XWiki xwiki;
-  private XWikiContext context;
-
-  @Before
-  public void setUp_DMSScriptServiceTest() throws Exception {
-    xwiki = createMock(XWiki.class);
-    context = getContext();
-    context.setWiki(xwiki);
-  }
+public class DMSScriptServiceTest extends AbstractComponentTest {
 
   @Test
   public void componentTest() {
-    replayAll();
+    replayDefault();
     assertNotNull(Utils.getComponent(ScriptService.class, "celdms"));
-    verifyAll();
-  }
-
-  //*************************************************************************************
-  // HELPER
-  //*************************************************************************************
-
-  
-  private void replayAll(Object ... mocks) {
-    replay(xwiki);
-    replay(mocks);
-  }
-
-  private void verifyAll(Object ... mocks) {
-    verify(xwiki);
-    verify(mocks);
+    verifyDefault();
   }
 
 }
